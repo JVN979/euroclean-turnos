83b7194e33b6ed651ee888891e2b064733a89cc1
JVN979 <todohogarmascotas@gmail.com>
2025-06-27 14:38:08 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
-// 💡 Reemplazá esta URL con la de tu propio Web App
 const URL = "https://script.google.com/macros/s/AKfycbxSBwEKb6C_VjQC1HlVmDbC_FZRFPN1W_4bLGbJT8CBZgIp9wsRm9saBrvkhx_HHYpk/exec";
 
-// Fecha con calendario
 const picker = new Pikaday({
   field: document.getElementById("fecha"),
   format: "YYYY-MM-DD",
@@ -22,29 +20,27 @@ function reservarTurno() {
     return;
   }
 
+  const formData = new FormData();
+  formData.append("fecha", fecha);
+  formData.append("hora", hora);
+  formData.append("nombre", nombre);
+  formData.append("vehiculo", vehiculo);
+  formData.append("observaciones", observaciones);
+
   fetch(URL, {
     method: "POST",
-    body: JSON.stringify({
-      fecha,
-      hora,
-      nombre,
-      vehiculo,
-      observaciones
-    }),
-    headers: {
-      "Content-Type": "application/json"
-    }
+    body: formData,
   })
-  .then(res => res.json())
-  .then(data => {
-    if (data.status === "ok") {
-      document.getElementById("respuesta").innerText = "✅ ¡Turno reservado con éxito!";
-    } else {
-      document.getElementById("respuesta").innerText = "❌ Ese turno ya está ocupado. Probá otro.";
-    }
-  })
-  .catch(err => {
-    console.error(err);
-    document.getElementById("respuesta").innerText = "⚠️ Error al enviar. Revisá la conexión.";
-  });
+    .then(res => res.json())
+    .then(data => {
+      if (data.status === "ok") {
+        document.getElementById("respuesta").innerText = "✅ ¡Turno reservado con éxito!";
+      } else {
+        document.getElementById("respuesta").innerText = "❌ Ese turno ya está ocupado. Probá otro.";
+      }
+    })
+    .catch(err => {
+      console.error(err);
+      document.getElementById("respuesta").innerText = "⚠️ Error al enviar. Revisá la conexión.";
+    });
 }
